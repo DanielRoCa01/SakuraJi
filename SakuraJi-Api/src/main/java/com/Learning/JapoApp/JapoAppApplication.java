868263743f3ff174427f0817337e6b2aae5e63f2
DanielRoCa01 <154538591+DanielRoCa01@users.noreply.github.com>
2025-05-29 21:07:02 +0200

    fix api change

--- SakuraJi-Api/src/main/java/com/Learning/JapoApp/JapoAppApplication.java
+++ SakuraJi-Api/src/main/java/com/Learning/JapoApp/JapoAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class cdJapoAppApplication {
+public class JapoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JapoAppApplication.class, args);
