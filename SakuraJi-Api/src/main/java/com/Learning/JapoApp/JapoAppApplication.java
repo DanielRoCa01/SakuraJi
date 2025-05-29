09b2af3faee859576110504924a4bca17105a7ae
DanielRoCa01 <154538591+DanielRoCa01@users.noreply.github.com>
2025-05-29 20:34:57 +0200

    env(web)change api url

--- SakuraJi-Api/src/main/java/com/Learning/JapoApp/JapoAppApplication.java
+++ SakuraJi-Api/src/main/java/com/Learning/JapoApp/JapoAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JapoAppApplication {
+public class cdJapoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JapoAppApplication.class, args);
